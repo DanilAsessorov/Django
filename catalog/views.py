4fe6ce3d62c1262e5ddfb0a061fb110378478500
данил <da_da_1914@list.ru>
2026-01-08 22:13:58 +0300

    Django

---
+++ catalog/views.py
@@ -0,0 +1,9 @@
+﻿from django.shortcuts import render
+
+def home(request):
+    """контроллер для главной страницы"""
+    return render(request, 'catalog/home.html')
+
+def contacts(request):
+    """контроллер для страницы контактов"""
+    return render(request, 'catalog/contacts.html')
