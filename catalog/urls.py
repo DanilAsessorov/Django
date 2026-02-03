f266ca4edee95dad58a428955a5e0d6b2116ed54
Данил <rusmaf14@gmail.com>
2026-02-03 03:20:51 +0300

    Revert "Django"

--- catalog/urls.py
+++
@@ -1,8 +0,0 @@
-﻿from django.urls import path
-from .views import HomeView, ContactsView, ProductDetailView
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),  # ← НОВЫЙ ПУТЬ
-]
